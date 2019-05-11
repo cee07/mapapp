96d6b11c4e6b87a3d1e7f9bb035efb2afc8c031f
JC Nuevas <ceenuevas@gmail.com>
2019-05-11 13:28:24 +0800

    Emergency Page Phone Call

---
+++ mapapp/Views/EmergencyContactView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace mapapp.Views {
+	public partial class EmergencyContactView : ContentView {
+		public EmergencyContactView () {
+			InitializeComponent();
+		}
+	}
+}
