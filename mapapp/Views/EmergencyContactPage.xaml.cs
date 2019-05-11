e71c06ceb16db956ea9457b9b7025f977e878c33
JC Nuevas <ceenuevas@gmail.com>
2019-05-11 13:28:33 +0800

    Emergency Page Phone Call

--- mapapp/Views/EmergencyContactPage.xaml.cs
+++ mapapp/Views/EmergencyContactPage.xaml.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using mapapp.ViewModels;
 using Xamarin.Forms;
 
 namespace mapapp.Views {
 	public partial class EmergencyContactPage : ContentPage {
+
+		private EmergencyViewModel emergencyViewModel;
+
 		public EmergencyContactPage () {
 			InitializeComponent();
+			BindingContext = emergencyViewModel = new EmergencyViewModel();
+
+			foreach (var emergencyModel in emergencyViewModel.EmergencyList) {
+				EmergencyContactView emergencyContactView = new EmergencyContactView(emergencyModel);
+				emergencyParent.Children.Add(emergencyContactView);
+			}
 		}
+
+
 	}
 }
--- mapapp/Views/EmergencyContactView.xaml.cs
+++ mapapp/Views/EmergencyContactView.xaml.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using mapapp.Models;
+using Plugin.Messaging;
 using Xamarin.Forms;
 
 namespace mapapp.Views {
 	public partial class EmergencyContactView : ContentView {
-		public EmergencyContactView () {
+
+		public EmergencyContactView (EmergencyModel model) {
 			InitializeComponent();
+			BindingContext = model;
+		}
+
+		void OnNumberTapped (object sender, System.EventArgs e) {
+			var label = (Label) sender;
+			if (label != null) {
+				var phoneDialer = CrossMessaging.Current.PhoneDialer;
+				if (phoneDialer.CanMakePhoneCall)
+					phoneDialer.MakePhoneCall(label.Text);
+				else
+					Application.Current.MainPage.DisplayAlert("Error",
+					                                          "Unable to make phone call",
+					                                          "OK");
+			}
 		}
 	}
 }
