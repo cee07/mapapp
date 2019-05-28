9b07e926060f6959f0d98990313b4f59486451d6
JC Nuevas <ceenuevas@gmail.com>
2019-05-28 15:12:22 +0800

    WIP Rate

---
+++ mapapp/Handlers/RateHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace mapapp.Handlers {
+	public class RateHandler : BaseDataHandler {
+
+		private JsonWebRequest<BaseDataModel> request;
+ 
+		public async Task Rate(string email, string crt, string establishmentID, string rating) {
+			APIForm apiForm = new APIForm();
+			apiForm.AddField("email", email);
+			apiForm.AddField("crt", crt);
+			apiForm.AddField("eid", establishmentID);
+			apiForm.AddField("rate", rating);
+		}
+
+		protected override Task OnAPICallSuccessful () {
+			throw new NotImplementedException();
+		}
+
+		protected override Task OnErrorOccured () {
+			throw new NotImplementedException();
+		}
+
+		protected override void OnTimedOut () {
+			throw new NotImplementedException();
+		}
+	}
+}
