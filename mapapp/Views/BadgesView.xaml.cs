a1af67e7e6622591404a4129065619a04294c9ae
JC Nuevas <ceenuevas@gmail.com>
2019-06-14 18:29:35 +0800

    Badges

--- mapapp/Views/BadgesView.xaml.cs
+++ mapapp/Views/BadgesView.xaml.cs
@@ -21,12 +21,22 @@ namespace mapapp.Views {
 
 		void OnInitializedBadges (List<BadgeModel> obj) {
 			badgesPanel.Children.Clear();
-			for (int index = 0 ; index < obj.Count ; index++) {
-				var badgeModel = obj[index];
-				if (badgeModel != null) {
-					BadgeValueView badgeValueView = new BadgeValueView(badgeModel);
-					badgesPanel.Children.Add(badgeValueView);
+			for (int index = 0 ; index < 7 ; index++) {
+				BadgeModel badgeModel = null;
+				if (index < obj.Count) {
+					badgeModel = obj[index];
+					if (badgeModel != null) {
+						int count = Convert.ToInt32(badgeModel.Count);
+						badgeModel.Badge = (string.IsNullOrEmpty(badgeModel.Badge)) ? string.Format("badge{0}.png", index.ToString()) : badgeModel.Badge;
+						badgeModel.Badge = (count > 0) ? badgeModel.Badge : string.Format("badge{0}.png", index.ToString());
+					}
+				} else {
+					badgeModel = new BadgeModel();
+					badgeModel.Badge = string.Format("badge{0}.png", index.ToString());
 				}
+				System.Diagnostics.Debug.WriteLine("Badges: " + badgeModel.Badge);
+				BadgeValueView badgeValueView = new BadgeValueView(badgeModel);
+				badgesPanel.Children.Add(badgeValueView);
 			}
 		}
 	}
